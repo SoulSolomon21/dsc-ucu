df3bc17f483ad50d044f6a5a7eed6263029afe4e
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-06 23:30:38 +0300

    testing custom useMyApi hook

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 
-export const useApi = (options, defaultValue) => {
+export const useApi = (options) => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://web-takehome-production.up.railway.app/api/signin', options);
-                const json = await response.json();
-                setData(json);
-            } catch (error) {
-                setError(error);
+        const fetchData = async () => await axios.post('https://web-takehome-production.up.railway.app/api/signin', myData).then(function (response) {
+            console.log(response)
+            if (response.data.memberData) {
+                alert("Successfully Signed In")
+                setData(response.data.memberData._doc)
+            } else {
+                setData(null)
+                setError(response.message)
             }
-        };
+        })
 
         fetchData();
     }, [options]);
@@ -21,30 +22,31 @@ export const useApi = (options, defaultValue) => {
     return [data, error];
 };
 
-export const useMyApi = (options, defaultValue) => {
-    const [data, setData] = useState(() => {
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const response = await fetch('https://web-takehome-production.up.railway.app/api/signin', options);
-                    const json = await response.json();
-                    setData(json);
-                } catch (error) {
-                    setError(error);
-                }
-            };
-    
-            fetchData();
-        }, [options]);
-    });
+export const useMyApi = () => {
+    const [data, setData] = useState({});
 
     const setApiData = (newData) => {
-        try {
-            
-        } catch (err) {
-            
+        if (newData) {
+            try {
+                useEffect(() => {
+                    const fetchData = async () => {
+                        try {
+                            const response = await fetch('https://web-takehome-production.up.railway.app/api/signin', newData);
+                            const json = await response.json();
+                            setData(json);
+                        } catch (error) {
+                            setError(error);
+                        }
+                    };
+                    fetchData();
+                }, [data]);
+            } catch (err) {
+                setData(null)
+            }
+        } else {
+            setData(null)
         }
     }
 
-    return [data, error];
+    return [data, setApiData];
 };
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,24 +1,29 @@
 import { createContext, useContext, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useLocalStorage } from "./useLocalStorage";
+// import { useLocalStorage } from "./useLocalStorage";
+import { useMyApi } from "./useApi";
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useLocalStorage("user", null)
+    // const [user, setUser] = useLocalStorage("user", null)
+    const [myUser,setMyUser] = useMyApi()
     const navigate = useNavigate()
 
     const login = async (data) => {
-        setUser(data)
+        // setUser(data)
+        setMyUser(data)
         navigate('/profile')
     }
 
     const logout = () => {
-        setUser(null)
+        // setUser(null)
+        setMyUser(null)
         navigate('/', { replace: true })
     }
 
     const value = useMemo(
         () => ({
+            myUser,
             user,
             login,
             logout
